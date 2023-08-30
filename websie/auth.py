5aed36b6e5fc558d02a7d3563091741641ca46cd
VS <anaberrantis@gmail.com>
2023-08-30 13:57:05 +0200

    Vytvorit Usra + pozriet vytovorenie databazy

--- websie/auth.py
+++ websie/auth.py
@@ -12,12 +12,12 @@ def logout():
 
 @auth.route('/sign-up' , methods=['GET', 'POST'])
 def sign_up():
-    if request.form == 'POST':
+    if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        
+                
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
@@ -27,6 +27,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            #Login
             flash('Congratulations. Account created!.', category='success')
         
     return render_template('sign_up.html')
